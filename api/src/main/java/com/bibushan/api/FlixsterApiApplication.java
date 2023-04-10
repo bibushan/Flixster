6f509da53cfd3223ea8d1b2e3a83a43bed0f8421
Bibushan <bibushansai@gmail.com>
2023-04-10 12:39:23 +0530

    initial commit

---
+++ api/src/main/java/com/bibushan/api/FlixsterApiApplication.java
@@ -0,0 +1,13 @@
+package com.bibushan.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlixsterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlixsterApiApplication.class, args);
+	}
+
+}
